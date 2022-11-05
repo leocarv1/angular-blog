59c8c06d9e7afa196819f0d7729912b374b355ea
leocarv <leo_1497@hotmail.com>
2022-11-05 09:53:17 -0300

    content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "saiu novo filme",
+        "description": "ola marvel anunciando novo filme do homem de ferro",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTniayDHJ_lpdP8VhNLimsUho9ibGmvBMInIg&usqp=CAU"
+    },
+    {
+        "id": "2",
+        "title": "saiu novo filme 2",
+        "description": "ola nova série, vamos assistir?",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+]
